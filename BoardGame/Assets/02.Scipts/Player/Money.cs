a5df1bf9f590e2c2b50d303f51d64e785cf05ed2
Woo Hyeok <pwh060720@gmail.com>
2024-05-03 23:07:59 +0900

    .

---
+++ BoardGame/Assets/02.Scipts/Player/Money.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BoardGame.Util;
+
+public class Money : GameComponent
+{
+    public Money(GameManager game) : base(game)
+    {
+    }
+
+    protected override void OnMove()
+    {
+        base.OnMove();
+    }
+}
