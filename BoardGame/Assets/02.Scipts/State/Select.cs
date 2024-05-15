e25e701208802fb398cfa955439e72d9c9c323c6
Woo Hyeok <pwh060720@gmail.com>
2024-05-15 22:31:56 +0900

    .

--- BoardGame/Assets/02.Scipts/State/Select.cs
+++ BoardGame/Assets/02.Scipts/State/Select.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BoardGame.Util;
+using System;
 
 public class Select : GameComponent
 {
@@ -16,37 +17,41 @@ public class Select : GameComponent
 
     protected override void OnSelect()
     {
-        if (manager.curBlock[manager.pTurn] % 10 == 0) StateMain();
+        StateMain();
+
+        if (manager.pTurn == PlayTurn.player)
+        {
+            UndoChoose(manager.buildCount[manager.curBlock[manager.pTurn]] == CurTower.none);
+            uiManager.ShowUI();
+        }
         else
         {
-            if (manager.buildCount[manager.curBlock[manager.pTurn]] != CurTower.none)
-                uiManager.ChooseActive(false);
-            else
-                uiManager.ChooseActive(true);
+            CurTower tower = manager.buildCount[manager.curBlock[manager.pTurn]];
 
-            if (manager.pTurn == PlayTurn.player)
-                uiManager.ShowUI();
+            int buildMoney = manager.buildingPrice[manager.curBlock[manager.pTurn]];
+            int money = manager.money[manager.pTurn];
+
+            if (money >= buildMoney * 3)
+                manager.tower = CurTower.tower03;
+            else if (money >= buildMoney * 2)
+                manager.tower = CurTower.tower02;
+            else if (money >= buildMoney)
+                manager.tower = CurTower.tower01;
+
+            if ((int)tower * buildMoney <= money)
+                manager.tower = tower;
+            else if ((int)tower * buildMoney * 2 <= money)
+                uiManager.isBuyBuilding = true;
             else
             {
-                CurTower tower = manager.buildCount[manager.curBlock[manager.pTurn]];
-                int buildMoney = manager.buildingPrice[manager.curBlock[manager.pTurn]];
-                int money = manager.money[manager.pTurn];
-
-                if ((int)tower * buildMoney <= money)
-                    manager.tower = tower;
-                else if((int)tower * buildMoney * 2 <= money)
-                    uiManager.isBuyBuilding = true;
-                else
-                {
-                    //그리고 파산 신청 상황
-                }
-
-                manager.UpdateState(GameState.Build);
+                //그리고 파산 신청 상황
             }
 
-            uiManager.UndoImg();
-            manager.isChangeColor = false;
+            manager.UpdateState(GameState.Build);
         }
+
+        uiManager.UndoImg();
+        manager.isChangeColor = false;
         //PayMoney();
 
     }
@@ -58,11 +63,45 @@ public class Select : GameComponent
 
         for (int i = 0; i < 4; i++)
             manager.DicMoney((PlayTurn)i, price);
+    } //혹시 모르니 두기
+
+    private void SelectBuild()
+    {
+        CurTower tower = manager.buildCount[manager.curBlock[manager.pTurn]];
+        int buildMoney = manager.buildingPrice[manager.curBlock[manager.pTurn]];
+        int money = manager.money[manager.pTurn];
+
+        if (tower != CurTower.none)
+        {
+            if ((int)tower * buildMoney <= money)
+                manager.tower = tower;
+            else if ((int)tower * buildMoney * 2 <= money)
+                uiManager.isBuyBuilding = true;
+        }
+
+        for (int num = Enum.GetValues(typeof(CurTower)).Length; num > 0; num--)
+        {
+            if ((CurTower)num == CurTower.none)
+            {
+                //파산 하였을 때
+            }
+
+            if (money >= buildMoney * num)
+                manager.tower = (CurTower)num;
+        }
+    }
+
+    private void UndoChoose(bool value)
+    {
+        uiManager.ChooseActive(value);
     }
 
     private void StateMain()
     {
-        manager.NextTurn(manager.pTurn);
-        manager.UpdateState(GameState.Main);
+        if (manager.curBlock[manager.pTurn] % 10 == 0)
+        {
+            manager.NextTurn(manager.pTurn);
+            manager.UpdateState(GameState.Main);
+        }
     }
 }
